ad642b540e71ee1a927238f82a0c511d9d2c9fc2
VishalBhatD <60818841+vishalbhatd123456@users.noreply.github.com>
2020-02-20 23:08:26 +0530

    Create process_Example1.js

---
+++ process_Example1.js
@@ -0,0 +1,5 @@
+console.log(`Process Architechture: ${process.arch}`);
+console.log(`Process pid: ${process.pid}`);
+console.log(`Process Platform: ${process.platform}`);
+console.log(`Process Version:${process.version}`);
+//console.log(`The CPU cpu Utilization is:${process.cpuUsage}`);
